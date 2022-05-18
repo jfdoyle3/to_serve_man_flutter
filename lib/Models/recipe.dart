3c57c4608518e687ded49bbfcf6239bfbc4cbe4f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-05-17 21:53:27 -0400

    working on recipe have some of it working

--- lib/Models/recipe.dart
+++ lib/Models/recipe.dart
@@ -2,15 +2,10 @@ import './ingredient.dart';
 
 
 class Recipe {
-    late final String recipeName;
-    late List<Ingredient> ingredients;
-    late List<String> steps;
+    final String recipeName;
+    List ingredients;
+    List steps;
 
-  void addIngredient(int amount, String measurement, String name){
-    ingredients.add(Ingredient(amount: amount, measurement: measurement, name: name));
-  }
+    Recipe(this.recipeName, this.ingredients, this.steps);
 
-  void addStep(String step){
-    steps.add(step);
-  }
 }
