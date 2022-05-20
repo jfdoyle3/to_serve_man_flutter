c4e9db256718396504ace0f7c35798f735756b97
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-05-19 23:27:56 -0400

    made a cookbook class. updated main

---
+++ lib/Models/cookbook.dart
@@ -0,0 +1,8 @@
+import '/Models/recipe.dart';
+
+class CookBook {
+  List<Recipe> cookbook = [];
+
+  CookBook.addRecipe(this.cookbook);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,51 @@ import 'Models/recipe.dart';
 
 
 void main() {
-   runApp(MyApp());
-  // Recipe cake=Recipe('cake',
-  //     [Ingredient(name: 'egg',amount: 1),
-  //      Ingredient(name: 'flour',amount: 4, measurement: 'cups'),],
-  //     ['mix ingredient','put batter in pan']);
-  //
-  // print(cake.recipeName);
-  //
-  // for(Ingredient item in cake.ingredients) {
-  //   int amount=item.amount;
-  //   String ingredientName=item.name;
-  //   String measurement=item.measurement;
-  //   print ('$amount $measurement $ingredientName');
-  // }
-  //
-  // for(String step in cake.steps) {
-  //   print(step);
-  // }
+  // runApp(MyApp());
+ Recipe cake = Recipe('Chocolate Cake', [
+    Ingredient(name: 'egg', amount: 1),
+    Ingredient(name: 'flour', amount: 4, measurement: 'cups'),
+  ], [
+    'mix ingredients',
+    'put batter in pan'
+  ]);
+
+  displayRecipe(cake);
+
+  Recipe pie = Recipe('Apple Pie', [
+    Ingredient(name: 'apple', amount: 10),
+    Ingredient(name: 'flour', amount: 5, measurement: 'cups'),
+    Ingredient(name: 'egg', amount: 1)
+  ], [
+    'mix ingredients',
+    'put apples and batter in pie plate'
+  ]);
+
+  print('');
+  print('CookBook');
+
+  CookBook myCookBook=CookBook.addRecipe([pie,cake]);
+  for(Recipe item in myCookBook.cookbook){
+    print(item.recipeName);
+  }
+}
+
+void displayRecipe(Recipe recipe) {
+  int stepNumber = 0;
+  print('Recipe:');
+  print(recipe.recipeName);
+  for (Ingredient item in recipe.ingredients) {
+    int amount = item.amount;
+    String ingredientName = item.name;
+    String measurement = item.measurement;
+    print('$amount $measurement $ingredientName');
+  }
+  print('----------------------');
+  print('Steps:');
+  for (String step in recipe.steps) {
+    stepNumber++;
+    print('$stepNumber. $step');
+  }
 }
 
 class MyApp extends StatefulWidget {
