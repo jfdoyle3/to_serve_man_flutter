f287dde8634984b4d69acbe929be379b70b14c9c
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-05-22 18:32:46 -0400

    imported cookbook object

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
 import 'Models/ingredient.dart';
 import 'Models/recipe.dart';
+import 'Models/cookbook.dart';
 
 
 void main() {
-  // runApp(MyApp());
- Recipe cake = Recipe('Chocolate Cake', [
-    Ingredient(name: 'egg', amount: 1),
-    Ingredient(name: 'flour', amount: 4, measurement: 'cups'),
-  ], [
-    'mix ingredients',
-    'put batter in pan'
-  ]);
-
-  displayRecipe(cake);
-
-  Recipe pie = Recipe('Apple Pie', [
-    Ingredient(name: 'apple', amount: 10),
-    Ingredient(name: 'flour', amount: 5, measurement: 'cups'),
-    Ingredient(name: 'egg', amount: 1)
-  ], [
-    'mix ingredients',
-    'put apples and batter in pie plate'
-  ]);
-
-  print('');
-  print('CookBook');
-
-  CookBook myCookBook=CookBook.addRecipe([pie,cake]);
-  for(Recipe item in myCookBook.cookbook){
-    print(item.recipeName);
-  }
+  runApp(MyApp());
+ // Recipe cake = Recipe('Chocolate Cake', [
+ //    Ingredient(name: 'egg', amount: 1),
+ //    Ingredient(name: 'flour', amount: 4, measurement: 'cups'),
+ //  ], [
+ //    'mix ingredients',
+ //    'put batter in pan'
+ //  ]);
+ //
+ //  displayRecipe(cake);
+ //
+ //  Recipe pie = Recipe('Apple Pie', [
+ //    Ingredient(name: 'apple', amount: 10),
+ //    Ingredient(name: 'flour', amount: 5, measurement: 'cups'),
+ //    Ingredient(name: 'egg', amount: 1)
+ //  ], [
+ //    'mix ingredients',
+ //    'put apples and batter in pie plate'
+ //  ]);
+ //
+ //  print('');
+ //  print('CookBook');
+ //
+ //  CookBook myCookBook=CookBook.addRecipe([pie,cake]);
+ //  for(Recipe item in myCookBook.cookbook){
+ //    print(item.recipeName);
+ //  }
 }
 
 void displayRecipe(Recipe recipe) {
