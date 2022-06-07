7b10f16ead864b7ebed0edf52b9eff9afd547b4f
jfdoyle3 <indellibleink@gmail.com>
2022-06-06 20:02:38 -0400

    creating recipe widget

--- lib/Models/recipe.dart
+++ lib/Models/recipe.dart
@@ -1,10 +1,35 @@
+import 'package:flutter/material.dart';
+
 import './ingredient.dart';
 
+class Recipe extends StatefulWidget {
+  final String recipeName;
+  List ingredients;
+  List steps;
 
-class Recipe {
-    final String recipeName;
-    List ingredients;
-    List steps;
+  Recipe(this.recipeName, this.ingredients, this.steps, {Key? key})
+      : super(key: key);
 
-    Recipe(this.recipeName, this.ingredients, this.steps);
+  @override
+  RecipeState createState() => RecipeState();
+}
+
+class RecipeState extends State<Recipe> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+            AppBar(backgroundColor: Colors.blue, title: const Text('Recipe')),
+        body: Column(
+          children: [
+            Container(
+              child: Row(
+               
+              ),
+            ),
+          ],
+        )
+    );
+  }
 }
