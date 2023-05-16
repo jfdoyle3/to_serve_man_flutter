da26398b0eeed4f1688208a20c471c9efa6359fd
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-05-15 21:32:39 -0400

    Working on state and updating cookbook

--- lib/src/Models/cookbook_model.dart
+++ lib/src/Models/cookbook_model.dart
@@ -5,8 +5,23 @@ import 'package:to_serve_man/src/Models/recipe.dart';
 
 class CookbookModel extends ChangeNotifier {
   final List<Recipe> _recipes = [];
+  late Recipe _recipe;
 
   UnmodifiableListView<Recipe> get recipes => UnmodifiableListView(_recipes);
+
+  /// The current catalog. Used to construct items from numeric ids.
+  Recipe get catalog => _recipe;
+
+  set recipe(Recipe newRecipe) {
+    _recipe = newRecipe;
+    // Notify listeners, in case the new catalog provides information
+    // different from the previous one. For example, availability of an item
+    // might have changed.
+    notifyListeners();
+  }
+
+  /// List of items in the cart.
+  List<Recipe> get items => _recipeId.map((id) => _recipe.getById(id)).toList();
 
   void add(Recipe recipe) {
     _recipes.add(recipe);
