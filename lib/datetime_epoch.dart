f700ce3fd1e5856ec1559422ae55a9fcddbe8063
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-06-21 06:41:07 -0400

    working with date time

--- lib/datetime_epoch.dart
+++ lib/datetime_epoch.dart
@@ -1,6 +1,14 @@
 main() {
   final now = DateTime.now();
   print(now);
-  final moonLanding = DateTime.parse('1969-07-20 20:18:04Z');
+  print(now.add(const Duration(days: 3)));
+  print(now.isUtc);
+  final utcTime =
+      now.toUtc().add(Duration(hours: now.timeZoneOffset.inHours)).toString();
+  print(now.timeZoneName);
+  print(now.timeZoneOffset);
+  print(utcTime);
+  // Date parsing by String
+  final moonLanding = DateTime.parse(utcTime);
   print(moonLanding.hour);
 }
