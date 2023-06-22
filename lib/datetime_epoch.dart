d22bc06de294fea91adba956e528c175a768d068
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-06-22 05:45:00 -0400

    working on getting unix time

--- lib/datetime_epoch.dart
+++ lib/datetime_epoch.dart
@@ -1,14 +1,27 @@
 main() {
-  final now = DateTime.now();
-  print(now);
-  print(now.add(const Duration(days: 3)));
-  print(now.isUtc);
-  final utcTime =
-      now.toUtc().add(Duration(hours: now.timeZoneOffset.inHours)).toString();
-  print(now.timeZoneName);
-  print(now.timeZoneOffset);
-  print(utcTime);
-  // Date parsing by String
-  final moonLanding = DateTime.parse(utcTime);
-  print(moonLanding.hour);
+  final utcTimeString = DateTime.now()
+      .toUtc()
+      .add(Duration(hours: DateTime.now().timeZoneOffset.inHours))
+      .toString();
+
+  print('String: ${utcTimeString}');
+
+  print(ConvertToEpochTime.dateToEpochTime());
+}
+
+class ConvertToEpochTime {
+  final utcTimeString = DateTime.now()
+      .toUtc()
+      .add(Duration(hours: DateTime.now().timeZoneOffset.inHours))
+      .toString();
+  final double MILLISECOND_TO_HOUR = 1000 * 60 * 60;
+  final double MILLISECOND_TO_DAY = 1000 * 60 * 60 * 24;
+  final double MILLISECOND_TO_MINUTE = 1000 * 60;
+
+  static dateToEpochTime() {
+    final parseDateTime = DateTime.parse(ConvertToEpochTime().utcTimeString);
+    final unixTime = parseDateTime.hour / 1000;
+
+    return unixTime;
+  }
 }
